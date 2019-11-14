e738218ecca37773022e28541b410f953ed8b70f
Kala Elam <speechgirl1505@gmail.com>
2019-11-13 18:02:21 -0600

    Updates

---
+++ precache-manifest.2c39c5c2a1109755f60ee5fb0d908e55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df19e0d0c54ca7996b3abc6f5003c097",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "ec40cd9d7e8be430e06a",
+    "url": "/Clicky-Game/static/css/main.5f856575.chunk.css"
+  },
+  {
+    "revision": "be5fae3f5e2b06ebe48b",
+    "url": "/Clicky-Game/static/js/2.d479714e.chunk.js"
+  },
+  {
+    "revision": "ec40cd9d7e8be430e06a",
+    "url": "/Clicky-Game/static/js/main.a81a9060.chunk.js"
+  },
+  {
+    "revision": "ac77e0aff6e05250fd73",
+    "url": "/Clicky-Game/static/js/runtime-main.0c69ceee.js"
+  }
+]);
